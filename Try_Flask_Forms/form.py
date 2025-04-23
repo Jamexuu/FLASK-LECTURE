2900290a01838f6df595b5bef7c9f1b28f5f74f1
Jamexuu <jamesmrrcado2209@gmaail.com>
2025-04-24 02:11:59 +0800

    added css

--- Try_Flask_Forms/form.py
+++ Try_Flask_Forms/form.py
@@ -2,9 +2,10 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, validators, EmailField, SubmitField
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username', [validators.Length(min=4, max=25)])
-    email = EmailField('Email Address', [validators.Length(min=6, max=35)])
-    password = PasswordField('Password', [validators.DataRequired(), validators.Length(min=8, max=35)])
-    confirm = PasswordField('Repeat Password', [validators.DataRequired(), validators.EqualTo('password', message='Passwords must match')])
+    username = StringField('Username', [validators.Length(min=4, max=25)], render_kw={"class": "username"})
+    email = EmailField('Email Address', [validators.Length(min=6, max=35)], render_kw={"class": "email"})
+    password = PasswordField('Password', [validators.DataRequired(), validators.Length(min=8, max=35)], render_kw={"class": "password"})
+    confirm = PasswordField('Repeat Password', [validators.DataRequired(), validators.EqualTo('password', message='Passwords must match')],
+                            render_kw={"class": "confirm"})
 
-    submit = SubmitField('Submit')
+    submit = SubmitField('Submit', render_kw={"class": "submit"})
